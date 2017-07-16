09ef41f94eaaafef8cf137a2a57b0aa9d8bdd612
charles <charles.bernard@cri-paris.org>
2017-07-16 20:10:07 +0200

    add print alignment function

--- Subscripts/HomoloBuild/main.c
+++ Subscripts/HomoloBuild/main.c
@@ -3,7 +3,6 @@ THIS SCRIPT TAKES 2 INPUT FILES:
 - FILE 1 MUST CONTAIN A LIST OF REFERENCE SEQUENCES
 - FILE 2 MUST CONTAIN A LIST OF SEQUENCES TO BE COMPARED 
 	 WITH EACH AND EVERY REFERENCE SEQUENCE
-
 THE OUTPUT IS A TABLE OF HOMOLOGY
 */
 
@@ -17,25 +16,13 @@ THE OUTPUT IS A TABLE OF HOMOLOGY
 #include "edlib.h"
 
 
+
+
 struct gene
 {
   char *id;
   char *seq;
   struct gene *next;
-};
-
-const int nbAmino = 24;
-struct blossum
-{
-  int scoreMatrix[nbAmino][nbAmino];
-  int minScore;
-  int maxScore;
-};
-
-struct fogsaaOut {
-  int score;
-  char *algA;
-  char *algB;
 };
 
 static void
@@ -53,11 +40,68 @@ usage(char *argv0)
 }
 
 void
+printAlignment(const char* query, const int queryLength,
+	       const char* target, const int targetLength,
+	       EdlibAlignResult result)
+{
+  int position = *result.endLocations;
+  unsigned char *alignment = result.alignment;
+  int alignmentLength = result.alignmentLength;
+  
+  int tIdx = -1;
+  int qIdx = -1;
+  for (int start = 0; start < alignmentLength; start += 50) {
+    // target
+    printf("T: ");
+    int startTIdx;
+    for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+      if (alignment[j] == EDLIB_EDOP_INSERT)
+	printf("-");
+      else
+	printf("%c", target[++tIdx]);
+      if (j == start)
+	startTIdx = tIdx;
+    }
+    if(startTIdx > 0)
+      printf(" (%d - %d)\n", startTIdx, tIdx);
+    else
+      printf(" (%d - %d)\n", 0, tIdx);
+
+    // match / mismatch
+    printf("   ");
+    for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+      printf(alignment[j] == EDLIB_EDOP_MATCH ? "|" : " ");
+    }
+    printf("\n");
+
+    // query
+    printf("Q: ");
+    int startQIdx = qIdx;
+    for (int j = start; j < start + 50 && j < alignmentLength; j++) {
+      if (alignment[j] == EDLIB_EDOP_DELETE)
+	printf("-");
+      else
+	printf("%c", query[++qIdx]);
+      if (j == start)
+	startQIdx = qIdx;
+    }
+    if(startQIdx > 0)
+      printf(" (%d - %d)\n", startQIdx, qIdx);
+    else
+      printf(" (%d - %d)\n", 0, qIdx);
+  }
+}
+
+void
 compare(struct gene *refList, struct gene *vsList)
 {
-  char *refSeq, *vsSeq;
-  int k = 0, refLen, vsLen, diffLen;
+  char *refSeq, *vsSeq, *bestSeq;
+  int k = 0, refLen, vsLen, bestLen, diffLen;
   struct gene *headVsList = vsList;
+
+  int score, bestScore;
+  char *bestId;
+  //EdlibAlignResult bestResult;
   
   while(refList != NULL) {
     
@@ -67,18 +111,48 @@ compare(struct gene *refList, struct gene *vsList)
     vsList = headVsList;
     while(vsList != NULL) {
 
+      bestScore = -1000;
+
       vsSeq = vsList -> seq;
       vsLen = strlen(vsSeq);
       
       diffLen = abs(refLen - vsLen);
       if(diffLen < (refLen / 4)) {
-	EdlibAlignResult result = edlibAlign(refSeq, refLen, vsSeq, vsLen, edlibDefaultAlignConfig());
-	printf("edit_distance(%s, %s) = %d\n", refList -> id, vsList -> id, result.editDistance);
+	//EdlibAlignResult result = edlibAlign(refSeq, refLen, vsSeq, vsLen, edlibDefaultAlignConfig());
+	EdlibAlignResult result = edlibAlign(refSeq, refLen, vsSeq, vsLen, edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH));
+	score = result.editDistance;
+	printf("%d\n", score);
+	printAlignment(refSeq, refLen, vsSeq, vsLen, result);
 	edlibFreeAlignResult(result);
-      }
 
+	/*
+	if(score > bestScore) {
+	  bestId = vsList -> id;
+	  bestScore = score;
+	  //edlibFreeAlignResult(bestResult);
+	  bestSeq = vsSeq;
+	  bestLen = vsLen;
+	  //EdlibAlignResult bestResult = result;
+	  edlibFreeAlignResult(result);
+	} else {
+	  edlibFreeAlignResult(result);
+	}
+	*/
+	
+      }
       vsList = vsList -> next;
     }
+
+    /*
+    printf("Best Score:%d\n", bestScore);
+    printf("RefId: %s;BestId: %s\n", refList -> id, bestId);
+    printf("RefSeq:%s\n", refSeq);
+    printf("BestSeq:%s\n", bestSeq);
+    */
+    
+    //printAlignment(refSeq, refLen, bestSeq, bestLen, bestResult);
+    //edlibFreeAlignResult(bestResult);
+    
     refList = refList -> next;
   }
 }
