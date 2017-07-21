4d8d8cc2187071b936ea736b7c81ec9274de4891
charles <charles.bernard@cri-paris.org>
2017-07-21 11:42:01 +0200

    add -m -w and -a options

--- Subscripts/HomoloBuild/main.c
+++ Subscripts/HomoloBuild/main.c
@@ -34,12 +34,18 @@ usage(char *argv0)
 	  "  -c: specify the file containing the list of sequences to be compared with the reference list.\n");
   fprintf(stderr,
 	  "  -o: specify the output file corresponding to the table of homology.\n");
+  fprintf(stderr,
+	  "  -m: method of alignment (NW, HW, SHW).\n");
+  fprintf(stderr,
+	  "  -w: write pretty alignment of best match.\n");
+  fprintf(stderr,
+	  "  -a: avoid Redundant Comparison with already assigned and certained homologuous.\n");
 }
 
 /* This function is there in case one would like to print the alignment
    btw two homologuous proteins */
 void
-writeAlignment(char *queryId, char* query, int queryLength,
+writePrettyAlignment(char *queryId, char* query, int queryLength,
 	       char *targetId, char* target, int targetLength,
 	       int position, unsigned char *alignment,
 	       int alignmentLength)
@@ -90,8 +96,25 @@ writeAlignment(char *queryId, char* query, int queryLength,
   }
 }
 
+/* if several vsSeqs happen to have the same best Score,
+   this function is there to concatenate their characteristics */
+char *
+concat(const char *s1, const char *s2)
+{
+    const size_t len1 = strlen(s1);
+    const size_t len2 = strlen(s2);
+    char *result = (char *)malloc(sizeof(char) * (len1+len2+2));//+2 for sep and zero-terminator
+    memcpy(result, s1, len1);
+    result[len1]=';';
+    memcpy(result+len1+1, s2, len2+1);//+1 to copy the null-terminator
+    return result;
+}
+
+/* This is the core function */
 void
-compare(char *filename, struct gene *refList, struct gene *vsList, int wa)
+  compare(EdlibAlignMode mode, char *filename,
+	  struct gene *refList, struct gene *vsList,
+	  int avoidRedundance, int writeAlignment)
 {
   int g = 1; int ng = lenList(refList);
   char *refSeq, *vsSeq, *bestSeq;
@@ -106,9 +129,14 @@ compare(char *filename, struct gene *refList, struct gene *vsList, int wa)
   int bestPosition;
   int bestAlignmentLength;
   unsigned char *bestAlignment = (unsigned char *)malloc(sizeof(unsigned char));
+  char *cigar = (char *)malloc(sizeof(char));
 
   FILE *f = fopen(filename, "w");
-  fprintf(f, "refId\tvsId\tedlibAlignmentScore\n");
+  if(f == NULL) {
+    printf("Impossible to open \"%s\"\n", filename);
+    exit(5);
+  }
+  fprintf(f, "refId\tvsId\tedlibAlignmentScore\tCigarAlignment\n");
   
   while(refList != NULL) {
 
@@ -119,7 +147,9 @@ compare(char *filename, struct gene *refList, struct gene *vsList, int wa)
     bestScore = refLen * 2;
     
     vsList = headVsList;
-    oldGene =  beforeBestGene = vsList;
+    if(avoidRedundance == 1) {
+      oldGene =  beforeBestGene = vsList;
+    }
           
     while(vsList != NULL) {
 
@@ -132,17 +162,34 @@ compare(char *filename, struct gene *refList, struct gene *vsList, int wa)
 
 	/* Here is just a reminder of how to run edlib by default: */
 	//EdlibAlignResult result = edlibAlign(refSeq, refLen, vsSeq, vsLen, edlibDefaultAlignConfig());
-	
+
+	// NB the major difference with main.c (for genes) is the "HW" infix EDLIB_MODE
 	EdlibAlignResult result = edlibAlign(refSeq, refLen, vsSeq, vsLen,
-					     edlibNewAlignConfig(-1, EDLIB_MODE_NW, EDLIB_TASK_PATH));
-	score = result.editDistance;	  
-	if(result.alignment && score < bestScore) {
+					     edlibNewAlignConfig(-1, EDLIB_MODE_HW, EDLIB_TASK_PATH));
+	score = result.editDistance;
+	
+	if(result.alignment && score <= bestScore) {
+	  
 	  /* Store variables relative to this new best target */
-	  bestScore = score;
-	  bestId = vsId;
-	  bestGene = vsList;
-	  beforeBestGene = oldGene;
-	  if(wa == 1) {
+	  if(score < bestScore) {
+	    bestId = vsId;
+	    cigar = edlibAlignmentToCigar(result.alignment,
+					  result.alignmentLength,
+					  EDLIB_CIGAR_EXTENDED);
+	  } else if(score == bestScore) {
+	    bestId = concat(bestId, vsId);
+	    cigar = concat(cigar,
+			   edlibAlignmentToCigar(result.alignment,
+						 result.alignmentLength,
+						 EDLIB_CIGAR_EXTENDED));
+	  }
+
+	  if(avoidRedundance == 1) {
+	    bestGene = vsList;
+	    beforeBestGene = oldGene;
+	  }
+	  
+	  if(writeAlignment == 1) {
 	    free(bestAlignment);
 	    bestSeq = vsSeq;
 	    bestLen = vsLen;
@@ -151,37 +198,47 @@ compare(char *filename, struct gene *refList, struct gene *vsList, int wa)
 	    bestAlignment = (unsigned char *)malloc(sizeof(unsigned char) * (bestAlignmentLength));
 	    memcpy(bestAlignment, result.alignment, (bestAlignmentLength));
 	  }
+	  
+	  bestScore = score;
 	}
 	edlibFreeAlignResult(result);
       }
-      oldGene = vsList;
+
+      if(avoidRedundance == 1) {
+	oldGene = vsList;
+      }
       vsList = vsList -> next;
     }
+    
     /* The task might be quite long, so a counter is necessary */ 
-    printf("\rtreated gene n°%d/%d", g, ng);
+    printf("\rTreated Query Seq n°%d/%d", g, ng);
     fflush(stdout);
     
     scoreLenRatio = float(bestScore) / float(refLen);
     if(scoreLenRatio < 1) { //0.5 is found to be a good threshold
-      if(wa == 1) {
-	writeAlignment(refId, refSeq, refLen, bestId, bestSeq, bestLen,
+      fprintf(f, "%s\t%s\t%.2f\t%s\n", refId, bestId, scoreLenRatio, cigar);
+
+      if(writeAlignment == 1) {
+	writePrettyAlignment(refId, refSeq, refLen, bestId, bestSeq, bestLen,
 		       bestPosition, bestAlignment, bestAlignmentLength);
       }
-      fprintf(f, "%s\t%s\t%.2f\n", refId, bestId, scoreLenRatio);
       
-      /* prevent redundant comparison with already assigned and certain homologuous */
-      if(scoreLenRatio < 0.1 && headVsList != bestGene) {
-	beforeBestGene -> next = bestGene -> next;
-	free(bestGene);
+      if(avoidRedundance == 1) {
+	if(scoreLenRatio < 0.1 && headVsList != bestGene) {
+	  beforeBestGene -> next = bestGene -> next;
+	  free(bestGene);
+	}
       }
 
     } else
-      fprintf(f, "%s\t.\t.\n", refId);
+      fprintf(f, "%s\t.\t.\t.\n", refId);
     
     refList = refList -> next;
     g++;
   }
+  free(cigar);
   free(bestAlignment);
+  printf("\n");
   fclose(f);
 }
 
@@ -191,10 +248,15 @@ main(int argc, char **argv)
   int i;
   char *refFile, *vsFile, *outFile;
   struct gene *refListGenes, *vsListGenes;
+
+  char *method = "NW";
+  EdlibAlignMode mode;
+
+  int writeAlignment = 0, avoidRedundance = 0;
   
   /* This scripts requires to be called with 7 args exactly:
   (1 is the script itself, 3 options and their 3 arguments) */
-  if(argc != 7) {
+  if(argc < 7) {
     usage(argv[0]);
     exit(0);
   }
@@ -205,34 +267,60 @@ main(int argc, char **argv)
       usage(argv[0]);
       exit(1);
     }
-    /* All options requires to specify an argument string (filename) */ 
+    i++;
+    /* All options requires to specify an argument string (filename)  
     i++;
     if(i >= argc) {
       usage(argv[0]);
       exit(2);
     }
-    /* Only 3 letters as options are allowed */
+    */
+    /* Only 6letters as options are allowed */
     switch(argv[i-1][1]) {
-    	case 'r':
-	  refFile = argv[i];
-	  break;
-    	case 'c':
-	  vsFile = argv[i];
-	  break;
-    	case 'o':
-	  outFile = argv[i];
-	  break;
-    	default :
-	  usage(argv[0]);
-	  exit(3); 
+    case 'r':
+      refFile = argv[i];
+      i++;
+      break;
+    case 'c':
+      vsFile = argv[i];
+      i++;
+      break;
+    case 'o':
+      outFile = argv[i];
+      i++;
+      break;
+    case 'm':
+      method = argv[i];
+      i++;
+      break;
+    case 'w':
+      writeAlignment = 1;
+      break;
+    case 'a':
+      avoidRedundance = 1;
+      break;
+    default :
+      usage(argv[0]);
+      exit(3); 
     }
-    i++;
   }
 
+  if(strcmp(method, "NW") == 0) {
+    mode = EDLIB_MODE_NW;
+  } else if(strcmp(method, "HW") == 0) {
+    mode = EDLIB_MODE_HW;
+  } else if(strcmp(method, "SHW") == 0) {
+    mode = EDLIB_MODE_SHW;
+  } else {
+    fprintf(stderr, "Wrong method of alignment!\n");
+    usage(argv[0]);
+    exit(4);
+  }
+      
   refListGenes = loadGenes(refFile);
   vsListGenes = loadGenes(vsFile);
 
-  compare(outFile, refListGenes, vsListGenes, 0);
+  compare(mode, outFile, refListGenes, vsListGenes, avoidRedundance, writeAlignment);
 
   freeList(refListGenes);
   freeList(vsListGenes);
